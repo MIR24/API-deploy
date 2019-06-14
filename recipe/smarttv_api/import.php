910895b8c6b41c5c2c8662b7bdf3e4ae9a1fcdb3
apsent86 <apsent86@gmail.com>
2019-06-14 13:12:56 +0300

    add bin/php

--- recipe/smarttv_api/import.php
+++ recipe/smarttv_api/import.php
@@ -4,17 +4,17 @@ namespace Deployer;
 
 desc('Import tables from mir24');
 task('import:mir24', function () {
-    $output = run('cd {{release_path}} &&  php artisan import:mir24:initial');
+    $output = run('cd {{release_path}} &&  {{bin/php}} artisan import:mir24:initial');
     writeln('<info>' . $output . '</info>');
-    $output = run('cd {{release_path}} &&  php artisan import:mir24');
+    $output = run('cd {{release_path}} &&   {{bin/php}} artisan import:mir24');
     writeln('<info>' . $output . '</info>');
 });
 
 
 desc('Import data for SmartTv and fix duration');
 task('import:smarttv', function () {
-    $output = run('cd {{release_path}} &&  php artisan import:smarttv');
+    $output = run('cd {{release_path}} &&   {{bin/php}} artisan import:smarttv');
     writeln('<info>' . $output . '</info>');
-    $output = run('cd {{release_path}} &&  php artisan import:video');
+    $output = run('cd {{release_path}} &&   {{bin/php}} artisan import:video');
     writeln('<info>' . $output . '</info>');
 });
