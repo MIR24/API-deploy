b78856c36d6ba351e9191c865554f70f55a5b2b1
IvanZ <apsent86@gmail.com>
2019-06-14 16:02:45 +0300

    add config:inject

--- recipe/smarttv_api/config.php
+++ recipe/smarttv_api/config.php
@@ -2,10 +2,15 @@
 
 namespace Deployer;
 
+function escapeForSed($value) {
+    return addcslashes($value, '/|&?!"\'');
+}
+
 desc('Add config for application (clone .env and app key)');
 task('smarttv_api:config', [
     'config:clone',
     'artisan:key:generate',
+    'config:inject',
 ]);
 
 desc('Propagate configuration file');
@@ -17,4 +22,15 @@ desc('Create app key');
 task('artisan:key:generate', function () {
     $output = run('cd {{release_path}} && {{bin/php}} artisan key:generate');
     writeln('<info>' . $output . '</info>');
+});
+
+desc('Propagate configuration file');
+task('config:inject', function () {
+    if(has('inject_env')){
+        $customEnv = get('inject_env', []);
+        foreach ($customEnv as $key => $value) {
+            $escapedValue = escapeForSed($value);
+            run("sed -i -E 's/^$key=.*/$key=$escapedValue/g' {{release_path}}/.env");
+        }
+    }
 });
